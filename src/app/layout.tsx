e081b382d38f12621ab2bd6fc2573a6a922e102b
will <emiliod255@gmail.com>
2025-08-08 22:46:01 -0400

    refactor: :recycle: RootLayout fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,13 +20,13 @@ export const metadata: Metadata = {
   description: "Creation of a todo list",
 };
 
-const locale = await getLocale();
-
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const locale = await getLocale();
+
   return (
     <html lang={locale}>
       <body className={`${poppins.variable}`}>
